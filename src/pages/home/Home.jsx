6a92d917aeeeafd8580e30fbe425bcf88eb61759
Rakib mia <rbepari404@gmail.com>
2024-04-08 11:26:55 -0600

    added all information

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,9 @@
-import CodeEditor from "../../components/codeEditor/CodeEditor";
 
 const Home = () => {
   return (
     <div>
-      {/* <h1 className="bg-red-800">home</h1> */}
-      <CodeEditor />
+      <h1>home</h1>
+      {/* <CodeEditor /> */}
     </div>
   );
 };
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/home/Home";
+import Blog from "../pages/blog/Blog";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
+import Profile from "../pages/profile/Profile";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/blog",
+    element: <Blog />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/dashboard/profile",
+    element: <Profile />,
   },
 ]);
 
